6feeb255bbfcb0454811659c11d38ff562304edf
charismaticstinger-dev <charismaticstinger.gmail.com>
2026-02-27 16:51:51 +0300

    js loops and function

---
+++ task5.js
@@ -0,0 +1,7 @@
+function myCountry(){
+    console.log("the London of Africa")
+    console.log("the cradle of mankind")
+    console.log("highest with human traficing")
+    console.log("with a green capital city")
+}
+myCountry()
